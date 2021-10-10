c3759848515115070943b8e34a2340f89c78a4c7
paula <paula.anna.wesolowska@gmail.com>
2021-10-10 18:58:52 +0200

    added first exercise

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.println("Witaj świecie!");
+        System.out.println("Uczymy się języka Java");
+        System.out.println("Życzę" + " " + "wam" + "miłej niedzieli");
+
+        System.out.printf("A %s zaczynamy z %d przykładem.\n", "więc", 1);
+
+        System.out.print("To jest przykładowy");
+        System.out.print(" tekst,");
+        System.out.print(" który nie jest za długi.");
 
     }
 }
