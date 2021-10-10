c17655b19f70d3eb2f5babba3015a58edf0b06ec
paula <paula.anna.wesolowska@gmail.com>
2021-10-10 20:16:35 +0200

    added sixth exercise

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,33 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        // pierwszy sposób
+        System.out.println("Tablica 1: ");
+        double[] tab1 = {1, 2, 3, 4, 5};
+        for (int i = 0; i < tab1.length; i++) {
+            System.out.println(tab1[i] + " ");
+        }
+
+        // drugi sposób
+        System.out.println("Tablica 2: ");
+        double[] tab2 = new double[5];
+        tab2[0] = 11;
+        tab2[1] = 12;
+        tab2[2] = 13;
+        tab2[3] = 14;
+        tab2[4] = 15;
+        for (int i = 0; i < tab2.length; i++) {
+            System.out.println(tab2[i] + " ");
+        }
+
+        //trzeci sposób
+        System.out.println("Tablica 3: ");
+        double[] tab3 = new double[5];
+        for (int i = 0; i < tab3.length; i++){
+            tab3[i] = i + 2.5;
+        }
+        for (int i = 0; i < tab3.length; i++) {
+            System.out.println(tab3[i] + " ");
+        }
     }
 }
